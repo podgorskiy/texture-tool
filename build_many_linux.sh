2a53fc15ac475e7a7b96878cd58f918be1d5452d
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-04-05 22:46:02 -0400

    docker writes output as root

--- build_many_linux.sh
+++ build_many_linux.sh
@@ -5,6 +5,9 @@ sudo rm -R wheelhouse
 
 docker run -it --rm -e PLAT=manylinux2014_x86_64 -v `pwd`:/io quay.io/pypa/manylinux2014_x86_64 sh /io/docker.sh
 
+me="$(whoami)"
+sudo chown "$me" -R wheelhouse
+
 # CXXABI_1.3.8 CXXABI_1.3.7
 
 
